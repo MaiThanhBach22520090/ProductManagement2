520d91d3a9a8d9a22de3676f872d3b3558b051f5
MaiThanhBach22520090 <22520090@gm.uit.edu.vn>
2024-10-28 21:17:13 +0700

    Add session-based login functionality
    Introduced a new login mechanism with session support:
    - Updated Program.cs to add and configure session services.
    - Added AccessController with Login actions for GET and POST.
    - Created Login.cshtml view for user authentication.
    - Redirect users based on roles after successful login.

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagement2.Models;
+
+namespace ProductManagement2.Controllers
+{
+	public class AccessController : Controller
+	{
+		QuanLySanPhamContext db = new QuanLySanPhamContext();
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			if (HttpContext.Session.GetString("Username") == null)
+			{
+				return View();
+			}
+			else
+			{
+				return RedirectToAction("Index", "Home");
+			}
+		}
+
+		[HttpPost]
+		public IActionResult Login(User user)
+		{
+			if (HttpContext.Session.GetString("Username") == null)
+			{
+				var obj = db.Users.Where(a => a.Username.Equals(user.Username) && a.PasswordHash.Equals(user.PasswordHash)).FirstOrDefault();
+				if (obj != null)
+				{
+					HttpContext.Session.SetString("Username", obj.Username);
+					HttpContext.Session.SetString("Role", obj.Role);
+					if (obj.Role == "Admin")
+					{
+						return RedirectToAction("ProductsManager", "Admin");
+					}
+					else
+					{
+						return RedirectToAction("Index", "Home");
+					}
+				}
+				else
+				{
+					ViewBag.error = "Invalid Account";
+					return View();
+				}
+			}
+			else
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+		}
+	}
+}
